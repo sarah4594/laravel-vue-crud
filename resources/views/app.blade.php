6ab99da1a1f19298d2e8d9df556b3b3a62f83fcf
sarah4594 <sarah4594@gmail.com>
2022-02-12 14:25:17 -0600

    Add tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" value="{{ csrf_token() }}" />
   <title>Vue JS CRUD Operations in Laravel</title>
-  <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+  <link href="/css/tailwind.css" type="text/css" rel="stylesheet" /> 
 </head>
 
 <body>
